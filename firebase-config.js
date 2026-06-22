4a066fe9601ea6c885a8dd7018ed63f24d72a259
neil <chuccam1011@gmail.com>
2026-06-22 23:47:46 +0700

    Add firebase config for GitHub Pages deployment

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Your Firebase configuration (KEEP THIS SECURE - do not commit to public Git)
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdYpOhwk2kTLI65ppr5a-SRIIDnOOXuXs",
+  authDomain: "revai-8f2e9.firebaseapp.com",
+  projectId: "revai-8f2e9",
+  storageBucket: "revai-8f2e9.firebasestorage.app",
+  messagingSenderId: "963879082947",
+  appId: "1:963879082947:web:974b53aebea7b6afecfb76"
+};
